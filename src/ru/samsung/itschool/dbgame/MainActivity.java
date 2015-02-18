03902aa303c5f66bac76aba6840879247eb10611
Taranow Maxim <tranow@mail.ru>
2015-02-18 18:55:22 +0400

    New comment

--- src/ru/samsung/itschool/dbgame/MainActivity.java
+++ src/ru/samsung/itschool/dbgame/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+//Коммент!!!
 	static DBManager dbManager;
 
 	EditText playerName;
