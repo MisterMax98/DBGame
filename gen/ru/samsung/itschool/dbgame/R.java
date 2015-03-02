bbefbdbc9ae4566e17b777d5a632e1768d23fe02
MisterMax <tranow@mail.ru>
2015-03-02 18:45:33 +0400

    Player Statistic

--- gen/ru/samsung/itschool/dbgame/R.java
+++ gen/ru/samsung/itschool/dbgame/R.java
@@ -31,24 +31,26 @@ public final class R {
         public static final int smallgreen=0x7f020003;
     }
     public static final class id {
-        public static final int GameResult=0x7f080004;
+        public static final int GameResult=0x7f080005;
+        public static final int LV=0x7f080001;
         public static final int clr=0x7f080000;
-        public static final int count=0x7f080008;
-        public static final int hallOfFame=0x7f080005;
-        public static final int max=0x7f08000b;
-        public static final int min=0x7f08000c;
-        public static final int plCount=0x7f080009;
-        public static final int playButton=0x7f080007;
-        public static final int playerName=0x7f080003;
-        public static final int results=0x7f080001;
-        public static final int stat=0x7f080006;
-        public static final int sum=0x7f08000a;
-        public static final int textView1=0x7f080002;
+        public static final int count=0x7f080009;
+        public static final int hallOfFame=0x7f080006;
+        public static final int max=0x7f08000c;
+        public static final int min=0x7f08000d;
+        public static final int plCount=0x7f08000a;
+        public static final int playButton=0x7f080008;
+        public static final int playerName=0x7f080004;
+        public static final int results=0x7f080002;
+        public static final int stat=0x7f080007;
+        public static final int sum=0x7f08000b;
+        public static final int textView1=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_ho_f=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_statistic=0x7f030002;
+        public static final int act_hof=0x7f030000;
+        public static final int activity_ho_f=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_statistic=0x7f030003;
     }
     public static final class string {
         public static final int action_settings=0x7f060004;
--- src/ru/samsung/itschool/dbgame/HoFActivity.java
+++ src/ru/samsung/itschool/dbgame/HoFActivity.java
@@ -1,13 +1,19 @@
 package ru.samsung.itschool.dbgame;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class HoFActivity extends Activity implements
@@ -15,27 +21,38 @@ public class HoFActivity extends Activity implements
 
 	private DBManager dbManager;
 	Button clr;
+	ListView lw;
 	TextView restv;
+	LinkedList<String> name;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_ho_f);
+		setContentView(R.layout.act_hof);
 		dbManager = DBManager.getInstance(this);
 		clr = (Button) findViewById(R.id.clr);
-		restv = (TextView) this.findViewById(R.id.results);
+		lw = (ListView) findViewById(R.id.LV);
+		clr.setOnClickListener(this);
 		loadRes();
 
-		clr.setOnClickListener(this);
+		ArrayAdapter<String> ad = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, name);
+
+		lw.setAdapter(ad);
+
+		// restv = (TextView) this.findViewById(R.id.results);
+		// loadRes();
+
 	}
 
 	void loadRes() {
+		name = new LinkedList<String>();
 		ArrayList<Result> results = dbManager.getAllResults();
 		String resStr = "";
 		for (Result res : results) {
-			resStr += res.name + ": " + res.score + "\n";
+			resStr = res.name + ": " + res.score + "\n";
+			name.add(resStr);
 		}
-		restv.setText(resStr);
 	}
 
 	@Override
